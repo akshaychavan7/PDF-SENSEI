622a177d3daa4fd7a954480fdb69bf5d0ebdd28b
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-12-01 11:04:11 -0600

    Complete code changes for chat with pdf

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://media.istockphoto.com/id/1445423940/vector/robot-head-icon-in-circle-vector-illustration.jpg?s=612x612&w=0&k=20&c=JiWZ5UfKlZ-l6DdGkeC4vOj_C8N8V56xz0hzd_EK8eE=" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://avatars.githubusercontent.com/u/39690337">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
